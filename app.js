dd7a72b298083a1950a0cceb7a653d8388693b7e
Albert-Byrone <albertbyrone1677@gmail.com>
2021-03-30 21:07:04 +0300

    minor imports

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+//database configurations
+//mongodb+srv://albertbyrone:<password>@cluster0.obns7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Thing = require('./models/thing');
+const app = express();
+
 mongoose.connect('mongodb+srv://albertbyrone:Albert254@cluster0.obns7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(()=>{
         console.log("Connected to the database");
