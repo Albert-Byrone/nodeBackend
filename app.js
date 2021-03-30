f39f988d6878965c6aa670424e36052d11d88c0b
Albert-Byrone <albertbyrone1677@gmail.com>
2021-03-30 20:50:39 +0300

    access one product from the database

--- app.js
+++ app.js
@@ -24,3 +24,14 @@ app.post('/api/stuff',(req, res, next)=>{
     });
 
 });
+app.get('/api/stuff/:id',(req,res, next)=>{
+    Thing.findOne({
+        _id: req.params.id
+    }).then((thing) =>{
+        res.status(200).json(thing)
+    }).catch((error) =>{
+        res.status(404).json({
+            error: error,
+        })
+    })
+})
