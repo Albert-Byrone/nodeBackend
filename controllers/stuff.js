bf11ad9940f8182b18a24e695c8bd791b27804a3
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-02 08:46:36 +0300

    delete a thing from the database

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -58,3 +58,17 @@ exports.getAllThings = (req,res, next)=>{
         });
     });
 };
+
+exports.deleteThing = (req, res, next) =>{
+    Thing.deleteOne({_id: req.params.id}).then(
+        ()=>{
+            res.status(200).json({
+                message: "Deleted"
+            });
+        }
+    ).catch(error =>{
+        res.status(400).json({
+            error: error
+        });
+    });
+};
