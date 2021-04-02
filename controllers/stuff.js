2be9fe0649b386fd354e1d44595fd0a9bdb64efa
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-02 08:46:03 +0300

    updata a thing in the databse

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -27,3 +27,25 @@ exports.getOneThing = (req,res, next)=>{
         })
     })
 };
+exports.updateOneThing =  (req, res, next)=>{
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId,
+
+    });
+    Thing.updateOne({_id:req.params.id}, thing).then(
+        ()=>{
+            res.status(201).json({
+                message: "Updates successfully",
+            });
+        }
+    ).catch(error =>{
+        res.status(400).json({
+            error: error
+        });
+    });
+};
