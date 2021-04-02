057df9786487ef4f3fd85291bcff01e0d2104ae1
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-02 08:50:23 +0300

    minor updates

--- routes/thing.js
+++ routes/thing.js
@@ -4,11 +4,11 @@ const Thing = require('../models/thing');
 const stuffController = require('../controllers/stuff');
 const router = express.Router();
 
-
+router.get('/', stuffController.getAllThings);
 router.post('/', stuffController.createThing);
 router.get('/:id', stuffController.getOneThing)
 router.put('/:id',stuffController.updateOneThing);
 router.delete('/:id', stuffController.deleteThing);
-router.get('/', stuffController.getAllThings);
+
 
 module.exports = router
