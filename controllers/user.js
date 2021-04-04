a08f548ab025244eee778266415be608baa4bfe8
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-04 20:35:18 +0300

    add login route and login

--- controllers/user.js
+++ controllers/user.js
@@ -23,3 +23,43 @@ exports.signup = (req, res, next)=> {
         });
     });
 }
+
+exports.login = (req, res, next) =>{
+    User.findOne({ email: req.body.email}).then(
+        (user)=>{
+            //check if the user exists in the database
+            if(!user){
+                return res.status(401).json({
+                    //authentication fail
+                    error: new Error("User does not exist")
+                });
+            }
+            //oif the user exist we compare the passwords
+            //compare the inputted passwword and the password in the database
+            bcrypt.compare(res.body.password, user.password).then(
+                (valid)=>{
+                    if(!valid){
+                        return res.status(401).json({
+                            error: new Error("Wrong password")
+                        })
+                    }
+                    //if the user exist retiurn the user
+                    res.status(200).json({
+                        userId: user._id,
+                        token: "token",
+                    })
+                }
+            ).catch(
+                (error) =>{
+                    res.status(500).json({
+                        error: error
+                    });
+                });
+
+        }).catch((error) =>{
+            res.status(500).json({
+                error: error
+            });
+        })
+
+};
