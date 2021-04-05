b94a5dedd7fbd63919200ac09085916236f76625
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-05 10:27:42 +0300

    install multer package

--- routes/thing.js
+++ routes/thing.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const auth = require('../middleware/auth')
 
 const Thing = require('../models/thing');
 const stuffController = require('../controllers/stuff');
 const router = express.Router();
 
-router.get('/', stuffController.getAllThings);
-router.post('/', stuffController.createThing);
-router.get('/:id', stuffController.getOneThing)
-router.put('/:id',stuffController.updateOneThing);
-router.delete('/:id', stuffController.deleteThing);
+router.get('/', auth, stuffController.getAllThings);
+router.post('/', auth,  stuffController.createThing);
+router.get('/:id',  auth, stuffController.getOneThing)
+router.put('/:id', auth, stuffController.updateOneThing);
+router.delete('/:id', auth,  stuffController.deleteThing);
 
 
 module.exports = router
