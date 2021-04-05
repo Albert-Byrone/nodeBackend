dfaa5184ef7e958524ae50074065304c876511c2
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-05 08:38:35 +0300

    install jwt package

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/user');
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+module.exports = router;
 
 
 module.exports = router;
