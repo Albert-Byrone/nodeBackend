4ab329b4e4ed2f1a17bbc1c48988754036829a2c
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-06 13:56:03 +0300

    configure the create function to add the image from the multer

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,12 +1,15 @@
 const Thing = require('../models/thing');
 
 exports.createThing = (req, res, next)=>{
+    const url = req.protocal + '://' + req.get('host');
+    console.log(url);
+    req.body.thing = JSON.parse(req.body.thing)
     const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId,
+        title: req.body.thing.title,
+        description: req.body.thing.description,
+        imageUrl: url + '/images/' + req.file.filename,
+        price: req.body.thing.price,
+        userId: req.body.thing.userId,
     });
     thing.save().then(()=>{
         res.status(201).json({
