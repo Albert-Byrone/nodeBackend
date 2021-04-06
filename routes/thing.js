bd780baab438654ddc3f73c9227fe782b7aa492c
Albert-Byrone <albertbyrone1677@gmail.com>
2021-04-06 13:57:54 +0300

    add multer to the routers

--- routes/thing.js
+++ routes/thing.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer_config');
 
 const Thing = require('../models/thing');
 const stuffController = require('../controllers/stuff');
 const router = express.Router();
 
 router.get('/', auth, stuffController.getAllThings);
-router.post('/', auth,  stuffController.createThing);
+router.post('/', auth, multer, stuffController.createThing);
 router.get('/:id',  auth, stuffController.getOneThing)
-router.put('/:id', auth, stuffController.updateOneThing);
+router.put('/:id', auth, multer, stuffController.updateOneThing);
 router.delete('/:id', auth,  stuffController.deleteThing);
 
 
